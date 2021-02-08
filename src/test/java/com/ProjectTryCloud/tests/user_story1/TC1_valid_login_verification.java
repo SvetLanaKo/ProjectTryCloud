bcdb252c17b53b167a9193386a92a9578671cc5b
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-08 17:35:59 -0600

    pushed one more time

--- src/test/java/com/ProjectTryCloud/tests/user_story1/TC1_valid_login_verification.java
+++ src/test/java/com/ProjectTryCloud/tests/user_story1/TC1_valid_login_verification.java
@@ -1,2 +1,33 @@
-package com.ProjectTryCloud.tests.user_story1;public class TC1_valid_login_verification {
+package com.ProjectTryCloud.tests.user_story1;
+
+import com.ProjectTryCloud.tests.base.TestBase;
+import com.ProjectTryCloud.utilities.TryCloudUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC1_valid_login_verification extends TestBase {
+
+
+
+        //1. Story: As a user, I should be able to login.
+        //Test case #1 - verify user login successfully
+
+        //NOTE: Before and After Methods are initialized in TestBase Class, this current Class is extended to that TestBase Class.
+    // steps are described below
+        @Test
+        public void login_verification_with_validCredentials() {
+            //NOTE: login credentials are stored in TryCloudUtils Java Class
+            //login steps with the valid credentials are described under loginPositive(driver) Method
+            //In this class, we will perform only URL verification
+
+            //5. Verify the URL is chawed to homepage's url --> chawed means "showed"
+            //ATTENTION: URL MUST BE CHANGED DUE TO NEW ENVIRONMENT
+            String expectedUrl = "http://qa3.trycloud.net";
+            String actualUrl = driver.getCurrentUrl();
+            Assert.assertTrue(actualUrl.contains(expectedUrl));
+        }
+
+
 }
