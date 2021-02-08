89458933438379b90edbefcfa3767c8d1700944c
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-07 21:04:11 -0600

    Initial commit, project was created

---
+++ src/test/java/com/ProjectTryCloud/tests/user_story1/TC1_valid_login_verification.java
@@ -0,0 +1,2 @@
+package com.ProjectTryCloud.tests.user_story1;public class TC1_valid_login_verification {
+}
---
+++ src/test/java/com/ProjectTryCloud/utilities/TryCloudUtils.java
@@ -0,0 +1,39 @@
+package com.ProjectTryCloud.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class TryCloudUtils {
+
+    public static void loginPositive(WebDriver driver){
+        // Enter valid username
+
+        WebElement userNameInputBox = driver.findElement(By.xpath("//input[@id='user']"));
+        userNameInputBox.sendKeys(ConfigurationReader.getProperty("username"));
+        //System.out.println(ConfigurationReader.getProperty("username"));
+        // Enter valid password
+        WebElement passwordInputBox = driver.findElement(By.xpath("//input[@id='password']"));
+        passwordInputBox.sendKeys(ConfigurationReader.getProperty("password"));
+        //System.out.println(ConfigurationReader.getProperty("password"));
+        // Click login button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@id='submit-form']"));
+        loginButton.click();
+
+    }
+    public static void loginNegative(WebDriver driver){
+        // Enter valid username
+        WebElement userNameInputBox = driver.findElement(By.xpath("//input[@id='user']"));
+        userNameInputBox.sendKeys(ConfigurationReader.getProperty("username"));
+        //System.out.println(ConfigurationReader.getProperty("username"));
+        // Enter valid password
+        WebElement passwordInputBox = driver.findElement(By.xpath("//input[@id='password']"));
+        passwordInputBox.sendKeys(ConfigurationReader.getProperty("invalidPassword"));
+        //System.out.println(ConfigurationReader.getProperty("password"));
+        // Click login button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@id='submit-form']"));
+        loginButton.click();
+
+    }
+}
